f7a9d65cd40df9487e7cdb5287ad744f2cc871fd
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-19 19:46:59 +0700

    Crate Operators.kt

---
+++ src/main/kotlin/com/erikriosetiawan/calculator/Operators.kt
@@ -0,0 +1,8 @@
+package com.erikriosetiawan.calculator
+
+object Operators {
+    fun add(a: Int, b: Int) = a + b
+    fun subtract(a: Int, b: Int) = a - b
+    fun multiply(a: Int, b: Int) = a * b
+    fun divide(a: Int, b: Int) = a / b
+}
