9885bcca655aacab3153ca872cb19d4391abee83
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-19 19:49:15 +0700

    Create Calculator.kt

--- src/main/kotlin/com/erikriosetiawan/calculator/Calculator.kt
+++ src/main/kotlin/com/erikriosetiawan/calculator/Calculator.kt
@@ -1,5 +1,8 @@
 package com.erikriosetiawan.calculator
 
-fun main() {
-    print("Hello, World!")
+class Calculator(private val operators: Operators) {
+    fun addTwoNumbers(a: Int, b: Int) = operators.add(a, b)
+    fun subtractTwoNumbers(a: Int, b: Int) = operators.subtract(a, b)
+    fun multiplyTwoNumbers(a: Int, b: Int) = operators.multiply(a, b)
+    fun divideTwoNumbers(a: Int, b: Int) = operators.divide(a, b)
 }
